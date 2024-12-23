605588e2e6492c3fc482a9005de096c9e2c98d43
MukarramHaq <mukarram796@gmail.com>
2024-12-23 14:04:02 -0500

    Working on the hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,25 @@ module.exports = {
       gunmetal: '#253347',
       'dark-electric-blue': '#5E6E85'
     },
-    fontSize: {
-      'heading-xl': ['64px', {lineHeight: '1.1'}],
-    },
     fontFamily: {
       'regular': 'Inter-Regular, sans-serif',
       'inter-semibold': 'Inter-SemiBold, sans-serif',
     },
-    extend: {},
+    boxShadow: {
+      main: 'shadow-[0px_4px_10px_8px_rgba(0,_0,_0,_0.1)]'
+    },
+    extend: {
+      fontSize: {
+        'heading-xl': ['64px', {lineHeight: '1.1'}],
+      },
+      height: {
+        'logo-sm': '36.72px',
+        17: '69px'
+      },
+      width: {
+        'logo-sm': '36.72px'
+      }
+    },
   },
   plugins: [],
 }
